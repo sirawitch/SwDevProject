fada46b3e0f28f1605eca3792bd1f803dfa84bab
Sirawitch Tiyasuttipun <57455984+sirawitch@users.noreply.github.com>
2023-02-18 14:09:17 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express= require('express');
+const dotenv=require('dotenv');
+const connectDB = require('./config/db');
+const cookieParser = require('cookie-parser')
+dotenv.config({path:'./config/config.env'});
+connectDB();
+const app=express();
+app.use(express.json());
+app.use(cookieParser())
+const PORT=process.env.PORT||5001;
+const server=app.listen(PORT,console.log('Server running in',process.env.NODE_ENV,'mode on port',PORT))
+process.on('unhandledRejection',(err,promise)=>{
+    console.log(`Error: ${err.message}`);
+    server.close(()=>process.exit(1));
+});
